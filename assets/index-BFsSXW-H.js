46196196464fcfae923ebbc5ec744d7764c61d0f
cassimgdc <easybrpix@gmail.com>
2026-02-21 19:50:13 -0300

    Deploy build React

---
+++ assets/index-BFsSXW-H.js
@@ -0,0 +1,1 @@
+import{r as u}from"./index-CC2g7BXp.js";function o(r){const e=u.useRef({value:r,previous:r});return u.useMemo(()=>(e.current.value!==r&&(e.current.previous=e.current.value,e.current.value=r),e.current.previous),[r])}export{o as u};
---
+++ assets/separator-D9ZVDhO7.js
@@ -0,0 +1,6 @@
+import{c as v,r as s,j as c,P as m,e as f}from"./index-CC2g7BXp.js";/**
+ * @license lucide-react v0.462.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const E=v("User",[["path",{d:"M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2",key:"975kel"}],["circle",{cx:"12",cy:"7",r:"4",key:"17ys0d"}]]);var u="Separator",n="horizontal",x=["horizontal","vertical"],l=s.forwardRef((r,a)=>{const{decorative:t,orientation:o=n,...e}=r,i=N(o)?o:n,d=t?{role:"none"}:{"aria-orientation":i==="vertical"?i:void 0,role:"separator"};return c.jsx(m.div,{"data-orientation":i,...d,...e,ref:a})});l.displayName=u;function N(r){return x.includes(r)}var p=l;const h=s.forwardRef(({className:r,orientation:a="horizontal",decorative:t=!0,...o},e)=>c.jsx(p,{ref:e,decorative:t,orientation:a,className:f("shrink-0 bg-border",a==="horizontal"?"h-[1px] w-full":"h-full w-[1px]",r),...o}));h.displayName=p.displayName;export{h as S,E as U};
