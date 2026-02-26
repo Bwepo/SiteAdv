f9bb7c549fb3f2da793f17271a4a50e4c72ce75f
cassimgdc <easybrpix@gmail.com>
2026-02-25 21:29:13 -0300

    Deploy build React

---
+++ assets/AnimatedImage-DRzdsSCD.js
@@ -0,0 +1,6 @@
+import{c as x,r as s,j as e,e as a}from"./index-sVkT8duI.js";import{u as j}from"./SEOHead-C6TnTx5y.js";/**
+ * @license lucide-react v0.462.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const b=x("Shield",[["path",{d:"M20 13c0 5-3.5 7.5-7.66 8.95a1 1 0 0 1-.67-.01C7.5 20.5 4 18 4 13V6a1 1 0 0 1 1-1c2 0 4.5-1.2 6.24-2.72a1.17 1.17 0 0 1 1.52 0C14.51 3.81 17 5 19 5a1 1 0 0 1 1 1z",key:"oel41y"}]]);function w({src:r,alt:i,className:c,containerClassName:d,priority:t=!1}){const[l,u]=s.useState(!1),[f,m]=s.useState(!1),n=s.useRef(null),v=j();s.useEffect(()=>{const o=new IntersectionObserver(([h])=>{h.isIntersecting&&(m(!0),o.disconnect())},{rootMargin:"100px",threshold:.1});return n.current&&o.observe(n.current),()=>o.disconnect()},[]);const g=()=>{u(!0)};return v?e.jsx("div",{className:a("overflow-hidden",d),children:e.jsx("img",{src:r,alt:i,className:c,loading:t?"eager":"lazy"})}):e.jsxs("div",{className:a("overflow-hidden relative",d),children:[e.jsx("img",{ref:n,src:f||t?r:void 0,alt:i,className:a(c,"w-full h-full object-cover"),loading:t?"eager":"lazy",onLoad:g}),!l&&e.jsx("div",{className:"absolute inset-0 bg-muted/30 animate-pulse pointer-events-none"})]})}export{w as A,b as S};
