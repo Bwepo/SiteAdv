a9bcf98034cb63f0fa074aebbeb5d415ff3a4b13
cassimgdc <easybrpix@gmail.com>
2026-06-15 16:57:59 -0300

    Deploy build React

---
+++ assets/skeleton-D4-W06vF.js
@@ -0,0 +1,1 @@
+import{j as t,a}from"./index-DxE62xRz.js";function m({className:e,...s}){return t.jsx("div",{className:a("animate-pulse rounded-md bg-muted",e),...s})}export{m as S};
--- assets/skeleton-DsqTYRQI.js
+++
@@ -1,1 +0,0 @@
-import{j as t,a}from"./index-D3c0lV5U.js";function m({className:e,...s}){return t.jsx("div",{className:a("animate-pulse rounded-md bg-muted",e),...s})}export{m as S};
